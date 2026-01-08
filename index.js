be221aa7636c887fd1fb7cda5572378bc6e363d5
Aashi Singh <aashisingh6566@gmail.com>
2026-01-08 15:05:32 +0530

    Add people entry counter project files

---
+++ index.js
@@ -0,0 +1,12 @@
+ let count = document.getElementById("count").innerText;
+ 
+ 
+ 
+ let count = 0;
+function increment() {
+    console.log("Button clicked");
+    count = count + 1;
+    console.log(count);
+}
+increment();
+increment();
